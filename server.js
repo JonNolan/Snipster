12c7ebcf0c2005f22d63af391c0549133814b1b9
JonNolanMCLA <a30106384@mcla.edu>
2020-09-23 00:36:13 -0400

    CSS fixes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const http = require('http');
 const fs = require('fs');
 const express = require("express");
 const session = require("express-session");
@@ -20,14 +21,38 @@ const sessionOptions = {
 };
 
 app.use(session(sessionOptions));
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 app.all("/", serveIndex);
-app.get("/snippets", getAndListSnippets);
-app.get("/filterSnippets", filterSnippets);
-app.get("/sortByCreator", sortSnippetsByCreator);
-app.get("/sortByLanguage", sortSnippetsByLanguage);
-app.get("/sortByDescription", sortSnippetsByDescription);
+app.get("/snippets", listSnippets);
+app.get("/filterCreator" , filterSnippetsByCreator);
+app.get("/filterLanguage", filterSnippetsByLanguage);
+app.get("/filterLanguageOrderCreatorAsc", filterSnippetsByLanguageOrderCreatorAsc);
+app.get("/filterLanguageOrderCreatorDesc", filterSnippetsByLanguageOrderCreatorDesc);
+app.get("/filterSnippetOrderCreatorAsc", filterSnippetsBySnippetOrderCreatorAsc);
+app.get("/filterSnippetOrderCreatorDesc", filterSnippetsBySnippetOrderCreatorDesc);
+app.get("/filterSnippetOrderLanguageAsc", filterSnippetsBySnippetOrderLanguageAsc);
+app.get("/filterSnippetOrderLanguageDesc", filterSnippetsBySnippetOrderLanguageDesc);
+app.get("/filterSnippetOrderDescriptionAsc", filterSnippetsBySnippetOrderDescriptionAsc);
+app.get("/filterSnippetOrderDescriptionDesc", filterSnippetsBySnippetOrderDescriptionDesc);
+app.get("/filterDescriptionOrderCreatorAsc", filterSnippetsByDescriptionOrderCreatorAsc);
+app.get("/filterDescriptionOrderCreatorDesc", filterSnippetsByDescriptionOrderCreatorDesc);
+app.get("/filterLanguageOrderDescriptionAsc", filterSnippetsByLanguageOrderDescriptionAsc);
+app.get("/filterLanguageOrderDescriptionDesc", filterSnippetsByLanguageOrderDescriptionDesc);
+app.get("/filterCreatorOrderLanguageAsc", filterSnippetsByCreatorOrderLanguageAsc);
+app.get("/filterCreatorOrderLanguageDesc", filterSnippetsByCreatorOrderLanguageDesc);
+app.get("/filterCreatorOrderDescriptionAsc", filterSnippetsByCreatorOrderDescriptionAsc);
+app.get("/filterCreatorOrderDescriptionDesc", filterSnippetsByCreatorOrderDescriptionDesc);
+app.get("/filterDescriptionOrderLanguageAsc", filterSnippetsByDescriptionOrderLanguageAsc);
+app.get("/filterDescriptionOrderLanguageDesc", filterSnippetsByDescriptionOrderLanguageDesc);
+app.get("/filterDescription", filterSnippetsByDescription);
+app.get("/filterSnippet", filterSnippetsBySnippet);
+app.get("/SortByCreator", sortSnippetsByCreator);
+app.get("/SortByLanguage", sortSnippetsByLanguage);
+app.get("/SortByDescription", sortSnippetsByDescription);
+app.get("/SortByCreatorByDesc", sortSnippetsByCreatorByDesc);
+app.get("/SortByLanguageByDesc", sortSnippetsByLanguageByDesc);
+app.get("/sortByDescriptionByDesc", sortSnippetsByDescriptionByDesc);
 app.listen(3000, process.env.IP, startHandler());
 
 const connection = mysql.createConnection(dbInfo);
@@ -62,6 +87,12 @@ function buildSnippet(dbObject) {
           Snippet: dbObject.Code};
 }
 
+function listSnippets(req, res) {
+  let ip = req.ip;
+  console.log(ip," is connecting");
+  getAndListSnippets(req, res);
+}
+
 function getAndListSnippets(req, res) {
   connection.query("SELECT * FROM Snippets;", function(err, dbResult) {
     if(err)
@@ -73,24 +104,74 @@ function getAndListSnippets(req, res) {
   });
 }
 
-// Filtering
-function filterSnippets(req, res) {
-  if (req.query.filter == undefined)
-    writeResult(res, {"error" : "Need to enter a filter!"});
-  let filter = "%" + req.query.filter + "%";
-  console.log(filter);
-  connection.query("SELECT * FROM Snippets WHERE Creator LIKE ? OR Lang LIKE ? OR Description LIKE ? OR Code LIKE ?", [filter, filter, filter, filter], function(err, dbResult) {
-    if(err)
-    writeResult(res, {error: err.message});
-    else {
-      let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet);});
-      console.log(snippets);
-      writeResult(res, {result: snippets});
-    }
-  });
+/// Filtering
+function filterSnippetsByCreator(req, res) {
+  if (req.query.creator == undefined) {
+    writeResult(res, {'error' : "Need to enter a Creator"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Creator LIKE ?", "%" + [req.query.creator] + "%", function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+      	writeResult(res, {result: snippets});
+      }
+    });
+  }
 }
-// Sorting
+
+function filterSnippetsByLanguage(req, res) {
+  if (req.query.language == undefined) {
+    writeResult(res, {'error' : "Need to enter a Language"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Lang LIKE ?", "%" + [req.query.language] + "%", function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+      	writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+function filterSnippetsByDescription(req, res) {
+  if (req.query.description == undefined) {
+    writeResult(res, {'error' : "Need to enter a Description"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Description LIKE ?", "%" + [req.query.description] + "%", function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+function filterSnippetsBySnippet(req, res) {
+  if (req.query.snippet == undefined) {
+    writeResult(res, {'error' : "Need to enter a Snippet"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Code LIKE ?", "%" + [req.query.snippet] + "%", function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+/// Sorting
 function sortSnippetsByCreator(req, res) {
+  let filter = '';
   connection.query("SELECT * FROM Snippets ORDER BY Creator", function(err, dbResult){
     if(err)
       writeResult(res, {error : err.message});
@@ -113,7 +194,7 @@ function sortSnippetsByLanguage(req, res) {
 }
 
 function sortSnippetsByDescription(req, res) {
-  connection.query("SELECT * FROM Snippets ORDER BY Description ASC", function(err, dbResult){
+  connection.query("SELECT * FROM Snippets ORDER BY CAST(Description AS UNSIGNED), Description ASC", function(err, dbResult){
     if(err)
       writeResult(res, {error : err.message})
     else {
@@ -121,4 +202,327 @@ function sortSnippetsByDescription(req, res) {
       writeResult(res, {result : snippets});
     }
   });
+}
+
+function sortSnippetsByCreatorByDesc(req, res) {
+  connection.query("SELECT * FROM Snippets ORDER BY Creator DESC", function(err, dbResult){
+    if(err)
+      writeResult(res, {error : err.message})
+    else {
+      let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+      writeResult(res, {result : snippets});
+    }
+  });
+}
+
+function sortSnippetsByLanguageByDesc(req, res) {
+  connection.query("SELECT * FROM Snippets ORDER BY Lang DESC", function(err, dbResult){
+    if(err)
+      writeResult(res, {error : err.message})
+    else {
+      let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+      writeResult(res, {result : snippets});
+    }
+  });
+}
+
+function sortSnippetsByDescriptionByDesc(req, res) {
+  connection.query("SELECT * FROM Snippets ORDER BY Description DESC", function(err, dbResult){
+    if(err)
+      writeResult(res, {error : err.message})
+    else {
+      let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+      writeResult(res, {result : snippets});
+    }
+  });
+}
+
+/// Filter and Sort
+function filterSnippetsByLanguageOrderCreatorAsc(req, res) {
+  if (req.query.language == undefined) {
+    writeResult(res, {'error' : "Need to enter a Language"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Lang LIKE ? ORDER BY Creator ASC", "%" + [req.query.language] + "%", function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+function filterSnippetsByLanguageOrderCreatorDesc(req, res) {
+  if (req.query.language == undefined) {
+    writeResult(res, {'error' : "Need to enter a Language"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Lang LIKE ? ORDER BY Creator DESC", "%" + [req.query.language] + "%", function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+function filterSnippetsByLanguageOrderDescriptionAsc(req, res) {
+  if (req.query.language == undefined) {
+    writeResult(res, {'error' : "Need to enter a Description"});
+  }
+  else {
+    console.log(ip," is filtering by Language");
+    connection.query("SELECT * FROM Snippets WHERE Lang LIKE ? ORDER BY Description ASC", "%" + [req.query.language] + "%", function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+function filterSnippetsByLanguageOrderDescriptionDesc(req, res) {
+  if (req.query.language == undefined) {
+    writeResult(res, {'error' : "Need to enter a Language"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Lang LIKE ? ORDER BY Description DESC", "%" + [req.query.language] + "%", function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+function filterSnippetsByCreatorOrderLanguageAsc(req, res) {
+  if (req.query.creator == undefined) {
+    writeResult(res, {'error' : "Need to enter a Creator"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Creator LIKE ? ORDER BY Lang ASC", "%" + [req.query.creator] + "%", function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+function filterSnippetsByCreatorOrderLanguageDesc(req, res) {
+  if (req.query.creator == undefined) {
+    writeResult(res, {'error' : "Need to enter a Creator"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Creator LIKE ? ORDER BY Lang DESC", "%" + [req.query.creator] + "%", function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+function filterSnippetsByCreatorOrderDescriptionAsc(req, res) {
+  if (req.query.creator == undefined) {
+    writeResult(res, {'error' : "Need to enter a Creator"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Creator LIKE ? ORDER BY Description ASC", "%" + [req.query.creator] + "%", function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+function filterSnippetsByCreatorOrderDescriptionDesc(req, res) {
+  if (req.query.creator == undefined) {
+    writeResult(res, {'error' : "Need to enter a Creator"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Creator LIKE ? ORDER BY Description DESC", "%" + [req.query.creator] + "%", function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+function filterSnippetsByDescriptionOrderCreatorAsc(req, res) {
+  if (req.query.description == undefined) {
+    writeResult(res, {'error' : "Need to enter a Description"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Description LIKE ? ORDER BY Creator ASC", "%" + [req.query.description] + "%",  function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+function filterSnippetsByDescriptionOrderCreatorDesc(req, res) {
+  if (req.query.description == undefined) {
+    writeResult(res, {'error' : "Need to enter a Description"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Description LIKE ? ORDER BY Creator DESC", "%" + [req.query.description] + "%", function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+function filterSnippetsByDescriptionOrderLanguageAsc(req, res) {
+  if (req.query.description == undefined) {
+    writeResult(res, {'error' : "Need to enter a Description"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Description LIKE ? ORDER BY Lang ASC", "%" + [req.query.description] + "%",  function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+function filterSnippetsByDescriptionOrderLanguageDesc(req, res) {
+  if (req.query.description == undefined) {
+    writeResult(res, {'error' : "Need to enter a Description"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Description LIKE ? ORDER BY Lang DESC", "%" + [req.query.description] + "%", function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+function filterSnippetsBySnippetOrderCreatorAsc(req, res) {
+  if (req.query.snippet == undefined) {
+    writeResult(res, {'error' : "Need to enter a Snippet"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Code LIKE ? ORDER BY Creator ASC", "%" + [req.query.snippet] + "%",  function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+function filterSnippetsBySnippetOrderCreatorDesc(req, res) {
+  if (req.query.snippet == undefined) {
+    writeResult(res, {'error' : "Need to enter a Snippet"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Code LIKE ? ORDER BY Creator DESC", "%" + [req.query.snippet] + "%", function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+function filterSnippetsBySnippetOrderLanguageAsc(req, res) {
+  if (req.query.snippet == undefined) {
+    writeResult(res, {'error' : "Need to enter a Snippet"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Code LIKE ? ORDER BY Lang ASC", "%" + [req.query.snippet] + "%",  function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+function filterSnippetsBySnippetOrderLanguageDesc(req, res) {
+  if (req.query.snippet == undefined) {
+    writeResult(res, {'error' : "Need to enter a Snippet"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Code LIKE ? ORDER BY Lang DESC", "%" + [req.query.snippet] + "%", function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+function filterSnippetsBySnippetOrderDescriptionAsc(req, res) {
+  if (req.query.snippet == undefined) {
+    writeResult(res, {'error' : "Need to enter a Snippet"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Code LIKE ? ORDER BY Description ASC", "%" + [req.query.snippet] + "%",  function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
+}
+
+function filterSnippetsBySnippetOrderDescriptionDesc(req, res) {
+  if (req.query.snippet == undefined) {
+    writeResult(res, {'error' : "Need to enter a Snippet"});
+  }
+  else {
+    connection.query("SELECT * FROM Snippets WHERE Code LIKE ? ORDER BY Description DESC", "%" + [req.query.snippet] + "%", function (err, dbResult) {
+      if (err)
+        writeResult(res, {error : err.message});
+      else {
+        let snippets = dbResult.map(function(snippet) {return buildSnippet(snippet)});
+        writeResult(res, {result: snippets});
+      }
+    });
+  }
 }
