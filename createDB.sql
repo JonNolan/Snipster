6762e8b5d6ddf79be2969cab08bdc271806c130b
JonNolanMCLA <a30106384@mcla.edu>
2020-09-24 01:15:56 -0400

    cleaned sql stuff. was messy

--- createDB.sql
+++ createDB.sql
@@ -14,72 +14,36 @@ CREATE TABLE Snippets(
 );
 
 INSERT INTO Snippets(Creator, Lang, Description, Code)
-VALUES ("Dylan", "SQL", "Wildcard", "SELECT * FROM Customers
-WHERE City LIKE 'ber%'; ");
+VALUES ("Dylan", "SQL", "Wildcard", "SELECT * FROM Customers WHERE City LIKE 'ber%';");
 INSERT INTO Snippets(Creator, Lang, Description, Code)
-VALUES ("Dylan", "SQL", "AUTO_INCREMENT", "CREATE TABLE Persons (
-  Personid int NOT NULL AUTO_INCREMENT,
-  LastName varchar(255) NOT NULL,
-  FirstName varchar(255),
-  Age int,
-  PRIMARY KEY (Personid)
-); ");
+VALUES ("Dylan", "SQL", "AUTO_INCREMENT", "CREATE TABLE Persons (Personid int NOT NULL AUTO_INCREMENT, LastName varchar(255) NOT NULL, FirstName varchar(255), Age int, PRIMARY KEY (Personid));");
 INSERT INTO Snippets(Creator, Lang, Description, Code)
-VALUES ("Dylan", "Java", "String Length", "var txt = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var sln = txt.length; ");
+VALUES ("Dylan", "Java", "String Length", "var txt = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; var sln = txt.length;");
 INSERT INTO Snippets(Creator, Lang, Description, Code)
 VALUES ("Dylan", "Python", "Casting", "Python Casting");
-
 INSERT INTO Snippets(Creator, Lang, Description, Code)
-VALUES ("Jason", "Java", "DB connect from Function Junction", "function buildSnippet(dbObject) {
-  return {creator: dbObject.User, language: dbObject.Language, description: dbObject.Description, Snip: dbObject.Snippet};}");
+VALUES ("Jason", "Java", "DB connect from Function Junction", "function buildSnippet(dbObject) {return {creator: dbObject.User, language: dbObject.Language, description: dbObject.Description, Snip: dbObject.Snippet};}");
 INSERT INTO Snippets(Creator, Lang, Description, Code)
-VALUES ("Jason", "C++", "Creating Pointers", "string food = 'Pizza'; 
- string* ptr = &food; ");
+VALUES ("Jason", "C++", "Creating Pointers", "string food = 'Pizza'; string* ptr = &food;");
 INSERT INTO Snippets(Creator, Lang, Description, Code)
-VALUES ("Jason", "Python", "Python Dictionaries", " thisdict =  {
-  'brand': 'Ford',
-  'model': 'Mustang',
-  'year': 1964
-}
-print(thisdict)");
+VALUES ("Jason", "Python", "Python Dictionaries", " thisdict =  {'brand': 'Ford', 'model': 'Mustang', 'year': 1964} print(thisdict)");
 INSERT INTO Snippets(Creator, Lang, Description, Code)
 VALUES ("Jason", "Linux", "Echo into file", " echo 'hello world' > world.txt");
 INSERT INTO Snippets(Creator, Lang, Description, Code)
-VALUES ("Jason", "Linux", "Get a list of all node process ids", "ps aux | grep node ");
-
+VALUES ("Jason", "Linux", "Get a list of all node process ids", "ps aux | grep node");
 INSERT INTO Snippets(Creator, Lang, Description, Code)
 VALUES ("Josh", "jQuery", "Hiding", " $(this).hide();");
 INSERT INTO Snippets(Creator, Lang, Description, Code)
 VALUES ("Josh", "Java", "Objects", "var car = {type:'Fiat', model:'500', color:'white'};");
 INSERT INTO Snippets(Creator, Lang, Description, Code)
-VALUES ("Josh", "Python", "If", "a = 33
-b = 200
-if b > a:
-  print('b is greater than a')");
+VALUES ("Josh", "Python", "If", "a = 33 b = 200 if b > a: print('b is greater than a')");
 INSERT INTO Snippets(Creator, Lang, Description, Code)
-VALUES ("Josh", "C++", "Switch", "switch(expression) {
-  case x:
-    break;
-  case y:
-    break;
-  default:
-}");
-
-
+VALUES ("Josh", "C++", "Switch", "switch(expression) {case x: break; case y: break; default: }");
 INSERT INTO Snippets(Creator, Lang, Description, Code)
 VALUES ("John", "Python", "Swap values between two variable", "a = 1 b = 2 a, b = b, a print(a) print");
 INSERT INTO Snippets(Creator, Lang, Description, Code)
 VALUES ("John", "Linux", "Change BASH prompt TEMP", "export PS1='\u >'");
 INSERT INTO Snippets(Creator, Lang, Description, Code)
-VALUES ("John", "C++", "Break", "for (int i = 0; i < 10; i++) {
-  if (i == 4) {
-    break;
-  }
-  cout << i << '\n';
-} ");
+VALUES ("John", "C++", "Break", "for (int i = 0; i < 10; i++) {if (i == 4) {break;} cout << i << '\n';}");
 INSERT INTO Snippets(Creator, Lang, Description, Code)
-VALUES ("John", "C#", "Get User Input", "// Type your username and press enter
-Console.WriteLine('Enter username:');
-string userName = Console.ReadLine();
-Console.WriteLine('Username is: '' + userName);");
+VALUES ("John", "C#", "Get User Input", "// Type your username and press enter Console.WriteLine('Enter username:'); string userName = Console.ReadLine(); Console.WriteLine('Username is: '' + userName);");
